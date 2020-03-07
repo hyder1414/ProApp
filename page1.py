f1e1c6a3841f03f789d9994a92abf80a30180f72
M. Haider Khan <34618387+hyder1414@users.noreply.github.com>
2020-03-07 15:27:45 -0600

    first commit

--- page1.py
+++ page1.py
@@ -1,5 +1,14 @@
 from tkinter import *
+from tkinter.ttk import Combobox
+
 root = Tk()
 root.title("ProApp")
+combo = Combobox(root)
+combo['values']= ("Java", "Python", "Algorithms", "Data Structure", "Interview", "Events")
+combo.current()
+combo.grid(column=2, row=4)
+
 
 root.mainloop()
+
+
